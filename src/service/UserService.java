50a92541c0810cf68239bbbd4f5718248dbb45b2
javaclass <javaclass@15000515cpu.fec.org>
2017-10-16 16:17:49 -0500

    3 tier arc

---
+++ src/service/UserService.java
@@ -0,0 +1,38 @@
+package service;
+
+import businessObjects.User;
+import myDAO.MockUserDAO;
+import utilities.PasswordUtilities;
+
+public class UserService {
+	
+	
+	public String saveUser(User user) {
+		
+		//mock user dao is a class inside the package dao
+		MockUserDAO userDao = new MockUserDAO();
+		
+//		if(userDao.retrieveUserByUsername(user.getUserName()) != null) {
+//			return "you are already a member";
+//		}
+	
+		
+		//if the password matches the criteria
+		//return string user has successfully registered
+		boolean isPasswordValid = PasswordUtilities.verifyPassword(user.getPassword());
+		if(isPasswordValid) {
+			
+			userDao.save(user);
+			return "You have successfully registered";
+		}
+		else {
+			return "Password entered is invalid";
+		}
+		
+		//if password fails the criteria
+		//return error
+		
+	}
+
+}
+
---
+++ src/userInterfaceFrontEnd/DummyUserApp.java
@@ -0,0 +1,32 @@
+package userInterfaceFrontEnd;
+
+
+import java.util.Scanner;
+
+import businessObjects.User;
+import service.UserService;
+
+
+public class DummyUserApp {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Please Enter Username:");
+		String userName = new Scanner(System.in).nextLine();
+		 
+		 System.out.println("Please Enter Password:");
+		 String password = new Scanner(System.in).nextLine();
+		 
+		 User user = new User();
+		 user.setUserName(userName);
+		 user.setPassword(password);
+		 
+		 UserService userService = new UserService();
+		 
+		 System.out.println(userService.saveUser(user));	
+		
+	}
+
+
+}
